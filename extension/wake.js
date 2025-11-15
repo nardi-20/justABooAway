09ff85fbab7e2de4581c0ccb89c5f74690507e96
Danie G. <danieog@umd.edu>
2025-11-15 16:06:10 -0500

    Add wake.js for interactive ghost greeting
    Introduces wake.js, which adds event listeners to elements for interactive behavior. Clicking the 'TombStone' element updates the greeting message and sets up a 'beforematch' event on the ghost element to display a 'Boo!' message.

---
+++ extension/wake.js
@@ -0,0 +1,18 @@
+const home = document.getElementById('TombStone');
+const message = document.getElementById('greetingMessage');
+const untilFound = document.querySelector("#until-found-ghost");
+
+
+home.addEventListener('click', function() {
+     message.textContent = 'Yawwwwnnnn';
+     aviable = true 
+     if (aviable){
+          //pop ghost up screen 
+          untilFound.addEventListener(
+               "beforematch",
+               () => (untilFound.textContent = "Boo!"),
+          );
+     }
+     message.textContent = 'Zzzzzzzzz...{unaviablemessage}';
+});
+
